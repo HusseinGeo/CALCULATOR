1108f8a70d9fc0f62beaf6f0c97467d33e93ba3e
Hussein mulla <mullaboxer@gmail.com>
2017-10-17 10:26:18 +0300

    add foldes and files

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var add = function(number1, number2) {
+	return number1 + number2;
+}
+
+alert(add(10, 5));
+
+var number1 = prompt("enter a number:");
+var number2 = prompt("enter another number:");
+
+alert(add(number1, number2));
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number2:"));
+
+alert(add(number1, number2));
